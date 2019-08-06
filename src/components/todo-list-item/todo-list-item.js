654912f25984d0177f33d4cc217681cbda25a07c
OlgaShitova <os@3d.ru>
2019-08-06 13:10:59 +0300

    добавила обработчик событий на статус таска

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -7,34 +7,55 @@ import './todo-list-item.css'
     */
 class ToDoListItem extends Component {
     /* наш класс  ToDoListItem является дочерним Components*/
-
+/*при клике меняем состояние */
+// здесь констурктор нужен только для того, что бы объявить клик по конкр элементу
     constructor () {
+        //если наш класс наследует какой-л класс, то мы указываем в constructor super()
         super();
-        this.onLabelClick =
-    }
-    onLabelClick() {
-        console.log(`clicked on ${this.props.label}`)
+        // стрелоч функция имеет наружн this - поэтому здесь function не подойдет
+        this.onLabelClick = () => {
+            console.log(`clicked on ${this.props.label}`)
+        }
     }
 
 
-    render() {
+
+    render() { // сюда прописать, то, что должна делать функция, без всякий состояний
         /* весь функционал прописываем внутри render*/
-        const { label, important = false } = this.props;
+        const { label, important = false } = this.props; 
+        //  если раньше они просто передавались в функцию, то теперь все необходимые свойства берем из this
+        //  строчка выше - это по сути наши props
         const style = {
             color: important ? 'tomato' : 'blue'
         }
 
         return (
             <span className="todo__list-item " >
-                <span className="todo__list-item-label " style={style} onClick={ this.onLabelClick.bind(this)}>
+                <span 
+                className="todo__list-item-label " 
+                style={style} 
+                onClick={ this.onLabelClick}>
                     {label}
                 </span>
-            </span>/*здесь применена деструктуризация */
+            </span>
         )
-        /* */
+        // выше привязали this с помощью bind, иначе оно теряется
     }
 }
 
-
+/**
+ * Функции и классы
+ * Если у нас нет состояния - то это функция, а если есть состояние - то это уже будет класс
+ * Переделка функции в класс:
+ * 
+ * Прописываем в верху файла в импорте { Component }
+ * Прописываем class Имя_класса_бывшее_имя_функции extends Component { 
+ * 
+ * закидываем сюда код функции
+ * вырезаем из функции параметы - они теперь берутся из this
+ * const { label, important = false } = this.props; 
+ * 
+ * 
+ */
 export default ToDoListItem;
 
